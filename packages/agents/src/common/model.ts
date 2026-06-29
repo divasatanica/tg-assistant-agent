cfb95f5e17593b83748ab0e2d5fe1bc0c41f9989
divasatanica <jayhebes@vip.qq.com>
2026-06-29 19:46:08 +0400

    refactor: use langgraph's mechanism to implement retry logic

--- packages/agents/src/common/model.ts
+++ packages/agents/src/common/model.ts
@@ -1,14 +1,7 @@
-import { OLLAMA_MODEL_NAME, GOOGLE_MODEL_NAME, GOOGLE_API_KEY } from '@krobert/utils/config';
-import { ChatOllama } from '@langchain/ollama';
+import { GOOGLE_MODEL_NAME, GOOGLE_API_KEY } from '@krobert/utils/config';
 import { ChatGoogle } from '@langchain/google';
 
-export function ollamaModelFactory(modelName: string = OLLAMA_MODEL_NAME) {
-  return new ChatOllama({
-    model: modelName,
-    temperature: 0,
-    numCtx: 128000,
-  });
-}
+const MODEL_MAX_RETRIES = 3;
 
 export function googleModelFactory(modelName: string = GOOGLE_MODEL_NAME) {
   return new ChatGoogle({
@@ -16,5 +9,6 @@ export function googleModelFactory(modelName: string = GOOGLE_MODEL_NAME) {
     temperature: 0,
     maxOutputTokens: 8192,
     apiKey: GOOGLE_API_KEY,
+    maxRetries: MODEL_MAX_RETRIES,
   });
 }
